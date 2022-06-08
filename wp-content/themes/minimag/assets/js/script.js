b8d30e53ac6cdd2783c5bb0b3bb9edb3f1028326
cwrsiqueira <cwrsiqueira@msn.com>
2022-06-08 02:29:38 -0300

    atualização

--- wp-content/themes/minimag/assets/js/script.js
+++ wp-content/themes/minimag/assets/js/script.js
@@ -2,27 +2,12 @@ document.querySelector('.main_footer_scroll').addEventListener('click', function
     window.scrollTo({top: 0, behavior: 'smooth'});
 });
 
-jQuery(function(){
+window.morePostsLoading = false;
+function loadMorePosts() {
+    
+    if(!window.norePostsLoading) {
+        window.norePostsLoading = true;
 
-    // jQuery(window).on('scroll', function(){
-    //     if (jQuery(window).scrollTop() + jQuery(window).height() >= jQuery(document).height()) {
-    //         jQuery('.loadmorebutton').show();
-    //         offset = jQuery('.postscontents article').length;
-    //         jQuery.ajax({
-    //             type:"post",
-    //             url:ajaxUrl,
-    //             data:{action:'LoadMorePosts', offset},
-    //             success:function(html){
-    //                 if(html != ''){
-    //                     jQuery('.postscontents').append(html)
-    //                 }
-    //                 jQuery('.loadmorebutton').hide()
-    //             }
-    //         })
-    //     }
-    // })
-
-    jQuery('.loadmorebutton').on('click', function(){
         jQuery('.loadmorebutton').hide();
         offset = jQuery('.postscontents article').length;
         jQuery.ajax({
@@ -30,13 +15,31 @@ jQuery(function(){
             url:ajaxUrl,
             data:{action:'LoadMorePosts', offset},
             success:function(html){
+                jQuery('.loadmorebutton').show()
                 if(html != ''){
                     jQuery('.postscontents').append(html)
-                    jQuery('.loadmorebutton').show()
                 } else {
                     jQuery('.loadmorebutton').hide()
                 }
+
+                window.norePostsLoading = false;
             }
         })
+    }
+
+}
+
+jQuery(function(){
+
+    jQuery('.loadmorebutton').on('click', loadMorePosts)
+
+    jQuery(window).on('scroll', function(){
+        var porcent = (window.scrollY / document.body.scrollHeight) * 100;
+        console.log(porcent)
+        if(porcent >= 50) {
+            loadMorePosts();
+        }
     })
+
+
 })
